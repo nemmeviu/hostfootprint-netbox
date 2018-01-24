593d693f4c086d5959abbfbf1e008bf668c97d04
nemmeviu <nemmeviu@blacktourmaline.com.br>
2018-01-24 15:36:13 -0300

    first dummy netbox api

---
+++ getnetworks.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import urllib3, json, os, sys
+http = urllib3.PoolManager()
+
+class NetboxAPI(object):
+    '''
+    NetboxAPI 
+    '''
+    def __init__(self):
+        try:
+            self.netbox_api_url = os.getenv('NETBOX_API_URL')
+        except:
+            self.netbox_api_url = 'http://localhost/api/'
+
+        # path API URLs
+        self.nb_api = {
+            # RIR Aggregates            
+            'rir': self.netbox_api_url + 'ipam/aggregates/',
+            # IPAM Prefixes
+            'prefix': self.netbox_api_url + 'ipam/prefixes/'
+        }
+
+    def json_import(self, nb_obj):
+        '''
+        Return json
+        '''
+        
+        json_nb = json.loads(nb_obj.data)
+        return(json_nb)
+
+    def get_nb_api(self, nb_target):
+        '''
+        Make the api request
+        '''
+
+        nb_target = self.nb_api[nb_target]
+
+        try:
+            print(nb_target)
+            enprint('GET ' + nb_target)
+            nb_result = http.request('GET', nb_target)
+        except MaxRetryError:
+            print('Fail to connect on: ' + nb_target)
+            sys.exit(2)
+
+        if nb_result.status == 200:
+            return(json_import(nb_result.data))
+
+#/home/msantago/Documents/blacktourmaline/github/hostfootprint-netbox
+
+test = NetboxAPI()
+
+print(test.get_nb_api('prefix'))
