b99ed13e1527accc70fd0ecb6f0366074ba4762e
nemmeviu <nemmeviu@blacktourmaline.com.br>
2018-03-14 22:58:09 -0300

    starting add pynmap and multiprocessing

--- getnetworks.py
+++ getnetworks.py
@@ -1,6 +1,97 @@
 #!/usr/bin/env python3
 from netboxapi import NetboxAPI
-import argparse
+import argparse, sys, time, os
+
+from multiprocessing import Manager
+#from multiprocessing import Pool
+from multiprocessing.pool import ThreadPool
+from threading import Thread, Lock
+
+import ipaddress, nmap
+
+index='nmap_v3'
+
+es_lock = Lock()
+es = ElsSaveMap(index, index)
+
+## ELASTICSEARCH index
+NMAPPROCS=int(os.getenv('NMAPPROCS'))
+HOSTSPROCS=int(os.getenv('HOSTSPROCS'))
+# windows = 'windows'
+# linux = 'linux'
+
+def syncronic():
+    return shared_info['sync']
+
+def get_hosts_and_clear():
+    result = []
+    while len(hosts_shared_lists) > 0:
+        result.append(hosts_shared_lists.pop())
+    return(result)
+
+def get_nets_and_clear():
+    result = []
+    while len(nets_shared_lists) > 0:
+        result.append(nets_shared_lists.pop())
+    return(result)
+
+def print_host(host_args):
+    es.es_save( *host_args )
+
+def do_print():
+    if syncronic():
+        hosts_args = get_hosts_and_clear()
+        for host_args in hosts_args:
+            es.es_save( *host_args )
+    else:
+        pool = ThreadPool(processes=HOSTSPROCS)
+        while not shared_info['finalizar'] or len(hosts_shared_lists) > 0:
+            hosts_args = get_hosts_and_clear()
+            if len(hosts_args) > 0:
+                pool.map(print_host, hosts_args )
+            time.sleep(1)
+
+# nmap
+def scan_net( subnet_object ):
+    nm = nmap.PortScanner()
+    nm.scan(
+        hosts=subnet_object['net'],
+        ports="445,22",
+        arguments="-P0 -n --open"
+    )
+
+    for host in nm.all_hosts():
+        # check if hosts exists:
+        with es_lock:
+            # ipaddress id on elasticsearch
+            ipid = "%s-%s" % (host, es.check_time())
+            body = {
+                "query": {
+                    "bool": {
+                        "must":{ "term": { "_id": ipid } }
+                    }
+                }
+            }
+                 
+            exist = es.client.search(
+                index=index,
+                doc_type=index,
+                body=body
+            )
+
+        try:
+            old = exist['hits']['hits'][0]['_source']['ip']
+        except:
+            if nm[host].has_tcp(445) is True:
+                hosts_shared_lists.append(
+                    ('windows', host, subnet_object['netobject'])
+                )
+            if nm[host].has_tcp(22) is True:
+                hosts_shared_lists.append(
+                   ('linux', host, subnet_object['netobject'])
+                )
+
+
 
 parser = argparse.ArgumentParser(
     description='Netbox API -> pynmap -> elasticsearch',
--- netboxapi.py
+++ netboxapi.py
@@ -285,7 +285,8 @@ class NetboxAPI(object):
 
                 nmap_list.append(nmap_object)
                 print(json.dumps(nmap_object, indent=4, sort_keys=True))
-            return(nmap_object)
+                
+            return(nmap_list)
             #print('elasticsearch save :)')
 
     def output_prefix(self, output):
